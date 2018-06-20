fcd1740edf16801efa0a08968f646bfaf7df9dd6
Tashbekova <tashbekovae@gmail.com>
2018-06-21 00:14:34 +0600

    b

--- django/mysite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^$', include('mainApp.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^web/', include('web.urls')),
-]
