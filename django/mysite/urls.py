6b51dc6bd71de6d8d27015a7fc151fa8199f5df1
Tashbekova <tashbekovae@gmail.com>
2018-06-20 17:35:08 +0600

    добавила шаблон

--- django/mysite/urls.py
+++ django/mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('mainApp.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^web/', include('web.urls')),
 ]
