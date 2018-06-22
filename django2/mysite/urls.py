01601ebac950f31fc0e78f67edf6579c1bdf17fc
Tashbekova <tashbekovae@gmail.com>
2018-06-22 15:08:41 +0600

    работает голосование

--- django2/mysite/urls.py
+++ django2/mysite/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('mainApp.urls')),
+    url(r'^', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^web/', include('web.urls')),
     url(r'^polls/', include('polls.urls')),
