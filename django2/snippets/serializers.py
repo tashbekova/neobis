4ea97ba77edb1e9ed45125ae66cbe6f2b7d3f75f
Tashbekova <tashbekovae@gmail.com>
2018-07-01 20:36:01 +0600

    добавила аутентификацию

--- django2/snippets/serializers.py
+++ django2/snippets/serializers.py
@@ -13,6 +13,7 @@ class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        owner = serializers.ReadOnlyField(source='owner.username')
    
     def create(self, validated_data):
         """
--- django2/snippets/urls.py
+++ django2/snippets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
@@ -10,3 +11,7 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls')),
+]
