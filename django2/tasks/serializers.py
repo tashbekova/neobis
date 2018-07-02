646d60dbda4dac4f80301fdfb041bbd639ccce5b
Tashbekova <tashbekovae@gmail.com>
2018-07-02 11:33:05 +0600

    добавила модели и сериализатор

--- django2/tasks/serializers.py
+++ django2/tasks/serializers.py
@@ -1,38 +1,35 @@
 from rest_framework import serializers
-from snippets.models import Task,, LANGUAGE_CHOICES, STYLE_CHOICES
+from tasks.models import Category,Branch,Contact,Course
 
-class TaskSerializer(serializers.Serilizer):
-    {
-    id=serializers.IntegerField(read_only=True)
-    name=serializers.CharField(required=False, allow_blank=True, max_length=100)
-    description=serializers.CharField(max_length=200)
-    category = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    logo=serializers.URLField()
-    "contacts": [
-        {
-            "type": "PHONE",
-            "value": "0770 792 299"
-        },
-        {
-            "type": "FACEBOOK",
-            "value": "https://www.facebook.com/english.zone.kg/"
-        },
-        {
-            "type": "EMAIL",
-            "value": "ezone.kg@gmail.com"
-        }
-    ],
-    "branches": [
-        {
-            "address": "Manas 58/ Aini - right next to the Manas university",
-            "address": "Manas 58/ Aini - right next to the Manas universit",
-            "latitude": "42.847971",
-            "longitude": "74.586733"
-},
-{
-            "address": "Бишкек, Юг-2 дом 15а Советская/Горького",
-            "latitude": "42.8586017",
-            "longitude": "74.6068425"
-        }
-    ]
-}
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name','imgpath')
+
+  class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = ('latitude', 'longtitude', 'addreaa')
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ('type', 'value')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    contacts = ContactSerializer(many=True)
+    branches = BranchSerializer(many=True)
+    category = serializers.ReadOnlyField(source='category.name')
+
+    class Meta:
+        model = Course
+        fields = ('name', 'category', 'description', 'contacts', 'logo', 'branches')
+
