7c620c8559bb82d6180d62b10c28c3a037533f55
Tashbekova <tashbekovae@gmail.com>
2018-07-10 13:55:07 +0600

    сделала миграцию

--- CRM_food/CRM/models.py
+++ CRM_food/CRM/models.py
@@ -1,3 +1,107 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model): 
+    department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True)
+
+    name = models.CharField(max_length=200)
+    
+    class Meta:
+        default_related_name="Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Department(models.Model):
+    name = models.CharField(max_length = 200)
+
+    class Meta:
+        verbose_name = "Department"
+        verbose_name_plural = "Departments"
+
+    def __str__(self):
+        return self.name
+
+class Meal(models.Model): 
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+
+    name = models.CharField(max_length=200)
+    price=models.CharField(max_length=200)
+    description=models.CharField(max_length=400)
+    
+    class Meta:
+        default_related_name="Meal"
+        verbose_name_plural = "Meals"
+
+    def __str__(self):
+        return self.name
+
+class MealsToOrder(models.Model): 
+    meal= models.ForeignKey('Meal', on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey('Order', on_delete=models.SET_NULL, null=True)
+
+    count = models.IntegerField(default=0)
+    
+    class Meta:
+        default_related_name="Meal"
+        verbose_name_plural = "Meals"
+
+
+class Role(models.Model):
+    name = models.CharField(max_length = 200)
+
+    class Meta:
+        verbose_name = "Role"
+        verbose_name_plural = "Roles"
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model): 
+    role = models.ForeignKey('Role', on_delete=models.SET_NULL, null=True)
+
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    login = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    dateofadd = models.DateTimeField(auto_now_add=True)
+    phone=models.CharField(max_length=50)
+    
+    class Meta:
+        default_related_name="User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model): 
+    waiter = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    table = models.ForeignKey('Table', on_delete=models.SET_NULL, null=True)
+    isitopen = models.ForeignKey('Status', on_delete=models.SET_NULL, null=True)
+    
+    date=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        default_related_name="Order"
+        verbose_name_plural = "Orders"
+
+class Status(models.Model):
+    name = models.CharField(max_length = 200)
+
+    class Meta:
+        verbose_name = "Status"
+        verbose_name_plural = "Statuses"
+
+    def __str__(self):
+        return self.name
+
+class Table(models.Model):
+    name = models.CharField(max_length = 200)
+
+    class Meta:
+        verbose_name = "Table"
+        verbose_name_plural = "Tables"
+
+    def __str__(self):
+        return self.name
